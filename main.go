427a9cb530a5a0b372c891ef45ee423e4919f2b2
Nathan Allen <nallenscott@me.com>
2023-12-18 06:44:07 -0500

    refactor `HandleRequest` function to accept a structured event

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest(ctx context.Context, name string) (string, error) {
-	return "Hello " + name, nil
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+	if event == nil {
+		return nil, fmt.Errorf("received nil event")
+	}
+	message := fmt.Sprintf("Hello %s!", event.Name)
+	return &message, nil
 }
 
 func main() {
